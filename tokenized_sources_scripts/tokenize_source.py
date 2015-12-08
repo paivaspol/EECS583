0729c69151026dadfac9f8c929b795a9d5026602
Andrew Quinn <aquinn1575@gmail.com>
2015-12-08 17:53:17 -0500

    tokenized the llvm sources

--- tokenized_sources_scripts/tokenize_source.py
+++ tokenized_sources_scripts/tokenize_source.py
@@ -21,114 +21,98 @@ def get_token(token):
         token_dict[token] = next_token
         next_token += 1
 
-    return token_dict[token]
-
-def tokenize_array(tokens, orig_string):
-    token_form = []
-    current_index = 0  #running total of current index.
-    
-    for i in range(len(tokens) -1):
-        #first deal with the current token
-        token = tokens[i]
-        current_index += len(token) 
-        next_str = str(get_token(token))
-        if next_str != "":
-            token_form.append(next_str)
-
-        #now deal with the split character
-        if current_index < len(orig_string):
-            split_char = orig_string[current_index]
-            current_index += 1
-            if split_char == "-" and current_index < len(orig_string) and (orig_string[current_index] == ">" or orig_string[current_index] == "=" or orig_string[current_index] == "-"):
-                split_char += orig_string[current_index]
-                current_index += 1
-            if split_char == "+" and current_index < len(orig_string) and (orig_string[current_index] == "=" or orig_string[current_index] == "+"):
-                split_char += orig_string[current_index]
-                current_index += 1
-            if split_char == "=" and current_index < len(orig_string) and orig_string[current_index] == "=":
-                split_char += orig_string[current_index]
-                current_index += 1
-            if split_char == ">" and current_index < len(orig_string) and (orig_string[current_index] == "=" or orig_string[current_index] == ">"):
-                split_char += orig_string[current_index]
-                current_index += 1
-            if split_char == "<" and current_index < len(orig_string) and (orig_string[current_index] == "=" or orig_string[current_index] == "<"):
-                split_char += orig_string[current_index]
-                current_index += 1
-            if split_char == "&" and current_index < len(orig_string) and orig_string[current_index] == "&":
-                split_char += orig_string[current_index]
-                current_index += 1
-            if split_char == "|" and current_index < len(orig_string) and orig_string[current_index] == "|":
-                split_char += orig_string[current_index]
-                current_index += 1
-
-
-            next_str = str(get_token(split_char))
-            if next_str != "":
-                token_form.append(next_str)
-
-
-
-    #didn't include the last index above b/c of endpoint issue
-    token = tokens[-1]
-    next_str = str(get_token(token))
-    if next_str != "":
-        token_form.append(next_str)
-        
-
-    return sep.join(token_form)
+    return str(token_dict[token])
         
 def tokenize_funct(function): 
     token_function = []
-    with open(function, "r") as funct_file:
-        comment = False
+    defined = False
+    switch = False
+    switch_text = []
+    with open(function, "r") as funct_file:        
         for line in funct_file:
-            if "*/" in line:
-                comment = False
+            #if there is a ':', it means that this line is a lable
+            line = line.strip()
+            if "define" in line and not defined:
+                defined = True
+
+#            elif "define" in line:
+#                print "uh oh... seems like I've already defined this!"
+#                print line            
+
+            elif "switch" in line:
+                switch = True
+                switch_text.append(line)
                 continue 
-            if "/*" in line:
-                comment = True
-                continue
-    
-            if "//" in line:
-                continue
-            if comment:
-                continue
-
-            #right now, dealing with the damn quotes!
-            if "\"" in line:
-                matches = [match.start() for match in re.finditer("\"",line)]
-                line = line[:matches[0]]+line[matches[-1]+1:]
-
-            if "\'" in line:
-                matches = [match.start() for match in re.finditer("\'",line)]
-                line = line[:matches[0]]+line[matches[-1]+1:]
                 
-            no_whitespace = line.split()
-               
-            for word in no_whitespace:              
-                split = re.split("\(|\)|\{|\}|\;|\.|\*|\-\>|\[|\]|\-\-|\-\=|\+\+|\+\=|\-|\+|\/|\=\=|\>|\<|\>\=|\<\=|\&|\&\&|\||\|\||\<\<|\>\>|\=|\,|\!|\#", word);
-                token_function.append(tokenize_array(split, word))
-                    
-    return sep.join(token_function)
+
+            elif "]" in line and switch:
+                switch_text.append(line)
+                backend = False
+                for line in switch_text:
+                    if "backedge" in line:
+                        token_function.append(get_token("back_sw"))
+                        backend = True
+                        break
+                
+                if not backend:
+                    token_function.append(get_token("sw"))
+                switch = False
+
+            elif switch:
+                switch_text.append(line)
+                continue
+
+            elif ":" in line or len(line) == 0 or line == "}" or "unreachable" in line: 
+                continue 
+
+            elif "br" in line:
+                if "backedge" in line:
+                    token_function.append(get_token("back_br"))
+                else: 
+                    token_function.append(get_token("br"))
+
+
+            elif "=" in line: 
+                split_equals = line.split("=")
+                operator = (split_equals[1].split()[0])
+                token_function.append(get_token(operator))
+        
+            elif "ret" in line:
+                token_function.append(get_token("ret"))
+
+            elif "store" in line:
+                token_function.append(get_token("store"))
             
+            #how should we deal with calls? 
+            elif "call" in line:
+                token_function.append(get_token("call"))
+            else: 
+                print "cannot classify line!"
+                print line
+                    
+
+    return sep.join(token_function)
 
 
 #check the syntax on that write call
 def tokenize_function(): 
     for benchmark in os.listdir(path):
-
         for cfiles in os.listdir(path + "/" + benchmark ):
             if not os.path.isdir(out_path + "/" + benchmark + "/" + cfiles):
                 os.makedirs(out_path + "/" + benchmark + "/" + cfiles)
 
             for function in os.listdir(path + "/" + benchmark + "/" + cfiles):
-                funct_token = tokenize_funct(path + "/" + benchmark + "/" + cfiles + "/" + function)
+#                print path + "/" + benchmark + "/" + cfiles + "/" + function
+                funct_tokens = tokenize_funct(path + "/" + benchmark + "/" + cfiles + "/" + function)
+
                 with open(out_path+"/"+benchmark+"/"+cfiles+"/"+function,"w+") as out_file:
-                    out_file.write(funct_token)
+                    out_file.write(funct_tokens)
             
+
+
 
 tokenize_function()
 
 print len(token_dict)
-#for item in token_dict:
-#    print item, token_dict[item]
+for item in token_dict:
+    print item, token_dict[item]
